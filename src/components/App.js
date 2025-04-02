36ff9541ce34e199b7e43d5c1d966ebf6ccbc92b
Shubham Pandit <Shubhamproduction1@gmail.com>
2025-04-02 13:23:50 +0530

    ans

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+
+  const [flag, setFlag] = useState(false)
   return (
     <div id="main">
       // Do not alter the main div
+      <h1>hubham</h1>
+      <button id='click' onClick={setFlag(true)}>Click</button>
+      {flag && <p>"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy."</p>}
     </div>
   );
 }
